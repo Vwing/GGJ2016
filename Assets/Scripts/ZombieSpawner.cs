0e564fde3105a4d9987f7c065c277edfcb8aa104
buckslice <buckslice@gmail.com>
2016-01-31 12:37:18 -0800

    adding zombies

---
+++ Assets/Scripts/ZombieSpawner.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class ZombieSpawner : NetworkBehaviour {
+
+    public GameObject zombiePrefab;
+
+    private float spawnRadius;
+    private int numZombies = 0;
+    private int maxZombies = 10;
+
+
+    void spawnZombie() {
+        Vector2 circ = Random.insideUnitCircle.normalized;
+        Vector3 spawn = new Vector3(circ.x, 0.0f, circ.y);
+        GameObject go = (GameObject)Instantiate(zombiePrefab, spawn, Quaternion.identity);
+    }
+
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
---
+++ Assets/Scripts/ZombieTarget.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class ZombieTarget : NetworkBehaviour {
+
+    private NavMeshAgent agent;
+    private Transform tform;
+    private Transform target;   // player
+
+
+	// Use this for initialization
+	void Start () {
+        agent = GetComponent<NavMeshAgent>();
+        tform = transform;
+        target = GameObject.Find("Player(Clone)").transform;
+	}
+	
+    void FixedUpdate() {
+        moveToTarget();
+    }
+
+    //void searchForTarget() {
+    //    if (!isServer) {
+    //        return;
+    //    }
+    //}
+
+	private void moveToTarget() {
+        if (target && isServer) {
+            agent.SetDestination(target.position);
+        }
+    }
+
+}
