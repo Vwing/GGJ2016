a2048da5dc877e954d926664ca4f954b3b51f499
Vwing <vwing@uci.edu>
2016-02-25 19:08:01 -0800

    Added custom image targets and resized stuff
    Only resized stuff and set things up in the local scenes (ARScene and
    VRScene) which I will be importing into the VRResearch project

---
+++ Assets/GetSize.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GetSize : MonoBehaviour {
+
+	[ContextMenu("Get bounding size of all child renderers")]
+	void GetSizeOfChildren() {
+        Renderer[] children = GetComponentsInChildren<Renderer>();
+        Bounds b = children[0].bounds;
+        for (int i = 1; i < children.Length; ++i)
+            b.Encapsulate(children[i].bounds);
+        Debug.Log("X bounds: " + b.size.x);
+        Debug.Log("Y bounds: " + b.size.y);
+        Debug.Log("Z bounds: " + b.size.z);
+	}
+	
+}
---
+++ Assets/ResizeChildren.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResizeChildren : MonoBehaviour {
+    public float ScaleMultiplier = 2f;
+    [ContextMenu("Resize all children by scale multiplier")]
+    void ResizeAll()
+    {
+        Transform[] children = GetComponentsInChildren<Transform>();
+        int len = children.Length;
+        for (int i = 1; i < len; ++i) //starts at 1 to skip parent.
+            children[i].localScale *= ScaleMultiplier;
+    }
+}
